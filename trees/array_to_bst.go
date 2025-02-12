cbd00af47e31addb9fb5e3095201a693c1c19dab
tano <tanoshkin@ya.ru>
2025-02-12 23:19:41 +0300

    Start array_to_bst

---
+++ trees/array_to_bst.go
@@ -0,0 +1,32 @@
+package trees
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	var result = &TreeNode{}
+	sliceToBST(nums, result)
+	return result
+}
+
+func sliceToBST(nums []int, node *TreeNode) {
+	numsLen := len(nums)
+	if numsLen <= 1 {
+		return
+	}
+	if numsLen == 2 {
+		node.Val = nums[1]
+		node.Left = &TreeNode{Val: nums[0]}
+		return
+	}
+	if numsLen == 3 {
+		node.Left = &TreeNode{Val: nums[0]}
+		node.Val = nums[1]
+		node.Right = &TreeNode{Val: nums[2]}
+		return
+	}
+	mid := numsLen / 2
+	node.Val = nums[mid]
+	node.Left = &TreeNode{}
+	node.Right = &TreeNode{}
+	sliceToBST(nums[:mid], node.Left)
+	sliceToBST(nums[mid+1:], node.Right)
+	return
+}
---
+++ trees/array_to_bst_test.go
@@ -0,0 +1,62 @@
+package trees
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_sortedArrayToBST(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		{
+			name: "1",
+			args: args{
+				nums: []int{-10, -3, 0, 5, 9},
+			},
+			want: func() *TreeNode {
+				root := &TreeNode{Val: 0}
+				root.Left = &TreeNode{Val: -3}
+				root.Left.Left = &TreeNode{Val: -10}
+				root.Right = &TreeNode{Val: 9}
+				root.Right.Left = &TreeNode{Val: 5}
+				return root
+			}(),
+		},
+		{
+			name: "2",
+			args: args{
+				nums: []int{1, 3},
+			},
+			want: func() *TreeNode {
+				root := &TreeNode{Val: 3}
+				root.Left = &TreeNode{Val: 1}
+				return root
+			}(),
+		},
+		{
+			name: "3",
+			args: args{
+				nums: []int{3, 5, 8},
+			},
+			want: func() *TreeNode {
+				root := &TreeNode{Val: 5}
+				root.Left = &TreeNode{Val: 3}
+				root.Right = &TreeNode{Val: 8}
+				return root
+			}(),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := sortedArrayToBST(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("sortedArrayToBST() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
