01aa4df44710fbe6ef3c60f47344c62f83c28fba
tano <tanoshkin@ya.ru>
2025-02-23 20:31:11 +0300

    finish maximum_subarray

--- dynamic/maximum_subarray.go
+++ dynamic/maximum_subarray.go
@@ -2,48 +2,12 @@ package dynamic
 
 import "math"
 
-type key struct {
-	i, j int
-}
-
 func maxSubArray(nums []int) int {
-	if len(nums) == 1 {
-		return nums[0]
+	bestSum := math.MinInt32
+	currentSum := 0
+	for _, num := range nums {
+		currentSum = max(num, currentSum+num)
+		bestSum = max(bestSum, currentSum)
 	}
-	sum := math.MinInt
-	cache := make(map[key]int, pow(len(nums), 2))
-	for i := 0; i < len(nums); i++ {
-		for j := len(nums); j >= i; j-- {
-			currentSum := calculateSum(i, j, nums, cache)
-			if currentSum > sum {
-				sum = currentSum
-			}
-		}
-	}
-	return sum
-}
-
-func calculateSum(begin int, end int, nums []int, cache map[key]int) int {
-	k := key{i: begin, j: end}
-	sum, ok := cache[k]
-	if ok {
-		return sum
-	} else {
-		result := 0
-		for i := begin; i < end; i++ {
-			curKey := key{begin, i}
-			v, curOK := cache[curKey]
-			if curOK {
-				return v
-			}
-			result += nums[i]
-			cache[curKey] = result
-		}
-		cache[k] = result
-		return result
-	}
-}
-
-func pow(a, b int) int {
-	return int(math.Pow(float64(a), float64(b)))
+	return bestSum
 }
